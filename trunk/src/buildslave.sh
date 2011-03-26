548717541e3e3ff35227377e0396827860dc88f4
shucai <shucai@f7b70b8b-449c-0410-839a-9a427d267331>
2011-03-26 00:14:41 +0000

    Remove unnecessary files.

--- trunk/src/buildslave.sh
+++
@@ -1,2 +0,0 @@
-#mpicc -L/home/shucaixiao/ati-stream-sdk-v2.2-lnx64/lib/x86_64 -lOpenCL -I/home/shucaixiao/ati-stream-sdk-v2.2-lnx64/include slaveOpenCL.c -o slave_process
-mpicxx -lOpenCL -lpthread -O3 -I/usr/local/cuda/include slaveOpenCL.c -o slave_process
